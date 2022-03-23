8d7f6a41de69b9e308bfc2924988da0744f51327
maalikh <maalikhorn@gmail.com>
2022-03-22 22:05:31 -0400

    add screenshots + update packages

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { AppState, getShowAllBooks } from './data/app.state';
-import { BooksDataService } from './shared/services/books-data.service';
+import { ChangeDetectorRef, Component } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { AppState, getShowAllBooks } from "./data/app.state";
+import { BooksDataService } from "./shared/services/books-data.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'convert-highlights-angular';
+  title = "convert-highlights-angular";
   public isMenuCollapsed = true;
   showAllBooks = true;
 
@@ -18,9 +18,11 @@ export class AppComponent {
     private booksDataService: BooksDataService,
     private cdr: ChangeDetectorRef
   ) {
-    this.store.select<boolean>(getShowAllBooks).subscribe((showAllBooks) => {
-      this.showAllBooks = showAllBooks;
-    });
+    this.store
+      .select<boolean>(getShowAllBooks)
+      .subscribe((showAllBooks: any) => {
+        this.showAllBooks = showAllBooks;
+      });
   }
 
   ngAfterViewChecked() {
